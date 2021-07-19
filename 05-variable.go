f6a542128338c228a519ef614cef98cfc5240c54
akimabs <kimuwhehe@gmail.com>
2021-07-20 00:05:26 +0700

    keren lahh, bikin variabel banyak tp kodingannya tetep keren ga kaya js koma2

---
+++ 05-variable.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Hakim" 
+
+	fmt.Println(name)
+	fmt.Println(len(name))
+	fmt.Println(name[0])
+
+	wilReturnNumber := 0
+
+	fmt.Println(wilReturnNumber)
+
+	var (
+		firstName string = "Abdul"
+		lastName string = "Hakim"
+	)
+
+	fmt.Println(firstName, lastName)
+ }
