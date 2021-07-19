9a4eabdfdf4dcdd21db25e1f6a1c998d9ada875c
akimabs <kimuwhehe@gmail.com>
2021-07-19 23:27:38 +0700

    tipedata(number)

---
+++ 02-number.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// int boleh ada minus 8, 16, 32, 64
+// uint mulai dari 0
+
+// float, number yang ada titik di belakangnya
+
+// Alias ini gokil
+// byte = uint8
+// rune = int32
+// int = minimal int32 (tergantung sistem operasinya)
+// uint = sama aja kaya int cuma mulai dari 0
+
+func main() {
+	fmt.Println("Satu", 1)
+	fmt.Println("Dua", 2)
+	fmt.Println("Tiga pake float", 3.5)
+}
